40e7d079b4d832b7907725f2ea214b3228e4a65b
KyleNiewiada <kniewia@gmail.com>
2015-04-12 23:15:25 -0400

    testing speed increase

--- config.rb
+++ config.rb
@@ -4,8 +4,10 @@ require "susy"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
+# css_dir = "assets/css"
+# sass_dir = "assets/scss"
+css_dir = "_includes/css"
+sass_dir = "_includes/scss"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
 
