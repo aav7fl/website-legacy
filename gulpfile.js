58bdf460d1a2f03feb3cbd39101a5782b46c119f
kyle <kniewia@gmail.com>
2017-01-11 12:08:40 -0500

    Improve gulp experience.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,18 @@ var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
 // Task for building blog when something changed:
-gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+gulp.task('build', shell.task(['bundle exec jekyll build --watch --profile']));
 
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
-    browserSync.init({server: {baseDir: '_site/'}});
+    browserSync.init({
+        server: {baseDir: '_site/'},
+        "notify": false,
+      });
     // Reloads page when some of the already built files changed:
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+    //Because I'm not using incremental, I can have it watch a single file.
+    //This prevents a plethora of reloads being sent.
+    gulp.watch('./_site/sitemap.xml').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['build', 'serve']);
