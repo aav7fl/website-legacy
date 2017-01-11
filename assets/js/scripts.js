1e9ed2fb845f904614d7aa3ada5a75e608734ca1
kyle <kniewia@gmail.com>
2017-01-11 15:00:51 -0500

    Remove old js dependencies.

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-	// Round Reading Time
-    $(".time").text(function (index, value) {
-      return Math.round(parseFloat(value));
-    });
-});
