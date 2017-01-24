b015e15df2cb3ed31b613a7ac20b0e4dd810e639
kyle <kniewia@gmail.com>
2017-01-24 11:05:29 -0500

    Change Travis Script. Add Sudo requirement

--- _script/build.sh
+++ _script/build.sh
@@ -3,11 +3,9 @@
 # Enable error reporting to the console.
 set -e
 
-echo 'bundle time'
 # Install bundles if needed.
 bundle check || bundle install
 # Build the site
-echo 'build site'
 bundle exec jekyll build
 
 # Checkout `master` and remove everything.
@@ -26,13 +24,8 @@ git config user.email ${GH_EMAIL}
 git config user.name "aavf7l-bot"
 
 # Commit and push generated content to `master` branch.
-echo 'git status'
 git status
-echo 'git add -A'
 git add -A .
-echo 'git status'
 git status
-echo 'git commit'
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-echo 'git push'
 git push --quiet origin `master` > /dev/null 2>&1
